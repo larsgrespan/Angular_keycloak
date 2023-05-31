1f68d6f5bb202ff368104b270f719ad4cd743587
Lars Grespan <larsgrespan@gmail.com>
2023-05-31 11:34:06 +0200

    auth guard

--- sso-app/src/app/auth.guard.ts
+++ sso-app/src/app/auth.guard.ts
@@ -1,12 +1,22 @@
-import { ActivatedRouteSnapshot, CanActivateFn, RouterStateSnapshot } from '@angular/router';
+import { Injectable } from "@angular/core";
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from "@angular/router";
+import { OAuthService } from "angular-oauth2-oidc";
 import { Observable } from 'rxjs';
-
-export const authGuard: CanActivateFn = (route, state) => {
   
-  return true;
+@Injectable({
+  providedIn: 'root'
+})
 
-};
+export class AuthGuard implements CanActivate {
 
+    constructor(
+        private oauthService: OAuthService) { }
 
-
-
+    canActivate(
+      route: ActivatedRouteSnapshot,
+      state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+        var hasIdToken = this.oauthService.hasValidIdToken();
+        var hasAccessToken = this.oauthService.hasValidAccessToken();
+        return (hasIdToken && hasAccessToken);
+        }
+}
